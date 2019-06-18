1847ea67360a99dbc47ad64148ce18f22477f3cc
Shanmuga <shanmuga.chidambaravel@gmail.com>
2019-06-18 22:38:18 +0200

    Added additional category group

--- flipkat_categories/spiders/flipkart_categories.py
+++ flipkat_categories/spiders/flipkart_categories.py
@@ -6,21 +6,22 @@ from ..items import CategoriesItem
 class FlipkartCategories(scrapy.Spider):
     name = "flipkart_categories"
     website = "flipkart.com"
+    required_category_groups = ["Clothing", "Clothing Trends"]
 
     start_urls = [
         'https://www.flipkart.com/sitemap',
     ]
 
     def parse(self, response):
-        clothing_category_links = response.xpath('//h2[a[text()="Clothing"]]/following-sibling::div[1]/a')
-        category_name = ["Clothing"]
-        for category_link in clothing_category_links:
-            record = CategoriesItem()
-            record['category_tree'] = category_name + [category_link.xpath("text()").extract_first()]
-            record['url'] = category_link.xpath("@href").extract_first()
-            record['website'] = self.website
-            yield record
-            yield scrapy.Request(response.urljoin(record['url']), callback=self.parse_filters_from_listing_page)
+        for category_group in self.required_category_groups:
+            clothing_category_links = response.xpath(f'//h2[a[text()="{category_group}"]]/following-sibling::div[1]/a')
+            for category_link in clothing_category_links:
+                record = CategoriesItem()
+                record['category_tree'] = [category_group] + [category_link.xpath("text()").extract_first()]
+                record['url'] = category_link.xpath("@href").extract_first()
+                record['website'] = self.website
+                yield record
+                yield scrapy.Request(response.urljoin(record['url']), callback=self.parse_filters_from_listing_page)
 
     def parse_filters_from_listing_page(self, response):
         js_script_data = response.xpath('//script[@id="is_script"]/text()').extract_first()
