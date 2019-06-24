594ed8792b104b780945382d514161057e13c46a
Shanmuga <shanmuga.chidambaravel@gmail.com>
2019-06-24 22:11:14 +0200

    Bug fix formed url from uri

--- flipkat_categories/spiders/flipkart_categories.py
+++ flipkat_categories/spiders/flipkart_categories.py
@@ -18,7 +18,7 @@ class FlipkartCategories(scrapy.Spider):
             for category_link in clothing_category_links:
                 record = CategoriesItem()
                 record['category_tree'] = [category_group] + [category_link.xpath("text()").extract_first()]
-                record['url'] = category_link.xpath("@href").extract_first()
+                record['url'] = response.urljoin(category_link.xpath("@href").extract_first())
                 record['website'] = self.website
                 yield record
                 yield scrapy.Request(response.urljoin(record['url']), callback=self.parse_filters_from_listing_page)
@@ -28,11 +28,11 @@ class FlipkartCategories(scrapy.Spider):
         # remove 'window.__INITIAL_STATE__ = ' from beginning and ';' from ending
         json_data = json.loads(js_script_data.strip()[27:-1])
         category_tree = json_data['pageDataV4']['browseMetadata']['storeMetaInfo']
-        heirarchy = [ct['title'] for ct in category_tree]
+        hierarchy = [ct['title'] for ct in category_tree]
         for x in category_tree[-1]['child']:
             record = CategoriesItem()
-            record['category_tree'] = heirarchy + [x['title']]
-            record['url'] = x['uri']
+            record['category_tree'] = hierarchy + [x['title']]
+            record['url'] = response.urljoin(x['uri'])
             record['website'] = self.website
             yield record
             yield scrapy.Request(response.urljoin(x['uri']), callback=self.parse_filters_from_listing_page)
